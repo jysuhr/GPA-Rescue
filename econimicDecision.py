305d8fa02fc4114fccc1b49d2326afdca6fc6e91
jysuhr <jun0.suhr@gmail.com>
2024-06-06 17:16:26 +0900

    입력창 구성 완료

--- econimicDecision.py
+++ econimicDecision.py
@@ -1,4 +1,5 @@
 from tkinter import*
+from tkinter import ttk
 root = Tk()
 root.geometry("640x480")
 root.title("경제적 의사결정 프로그램")
@@ -39,45 +40,81 @@ aLabel.grid(column=3, row=0)
 # ===================== 이름 =======================
 # 사업 이름 레이블
 nameLabel = Label(root, text = "사업 이름", font = ("맑은고딕", 16, "bold"))
-nameLabel.grid(column=0, row=1, sticky="w")
+nameLabel.grid(column=0, row=1, sticky="w", padx=10)
 
 # 이름 입력창
 newNameEntry = Entry(root, width = 15)
 newNameEntry.focus()
 newNameEntry.grid(column=1, row=1)
 
+space(2, 1)
+
+oldNameEntry = Entry(root, width = 15)
+oldNameEntry.focus()
+oldNameEntry.grid(column=3, row=1)
+
 
 # ===================== 투자금 =======================
 # 사업 투자금 레이블
 depositLabel = Label(root, text = "사업 투자금", font = ("맑은고딕", 16, "bold"))
-depositLabel.grid(column=0, row=2, sticky="w")
+depositLabel.grid(column=0, row=2, sticky="w", padx=10)
 
 # 투자금 입력창
 newDepositEntry = Entry(root, width = 15)
 newDepositEntry.grid(column=1, row=2)
 
+space(2, 2)
+
+oldDepositEntry = Entry(root, width = 15)
+oldDepositEntry.grid(column=3, row=2)
+
 
 # ===================== 이자 =======================
 # 사업 투자금 레이블
 interestLabel = Label(root, text = "사업 이자", font = ("맑은고딕", 16, "bold"))
-interestLabel.grid(column=0, row=3, sticky="w")
+interestLabel.grid(column=0, row=3, sticky="w", padx=10)
 
 # 투자금 입력창
 newInterestEntry = Entry(root, width = 15)
 newInterestEntry.grid(column=1, row=3)
 
+space(2, 3)
+
+oldInterestEntry = Entry(root, width = 15)
+oldInterestEntry.grid(column=3, row=3)
+
 # ===================== 기간 =======================
 # 사업 기간 레이블
 periodLabel = Label(root, text = "계약 기간", font = ("맑은고딕", 16, "bold"))
-periodLabel.grid(column=0, row=4, sticky="w")
+periodLabel.grid(column=0, row=4, sticky="w", padx=10)
 
 # 기간 입력창
 newPeriodEntry = Entry(root, width = 15)
 newPeriodEntry.grid(column=1, row=4)
 
-# ======================= 끝 =========================
+space(2, 4)
 
-# 사업 이름 추출 실험 =================================
+oldPeriodEntry = Entry(root, width = 15)
+oldPeriodEntry.grid(column=3, row=4)
+
+# ================== 가로 구분선 =====================
+separator = ttk.Separator(root, orient='horizontal')
+separator.grid(row=5, column=0, columnspan=5, sticky='ew', padx=(10, 0), pady=2)
+
+# NPV
+npv_label = Label(root, text = "NPV", font = ("맑은고딕", 16, "bold"))
+npv_label.grid(column=0, row=6, sticky="w", padx=10)
+
+# IRR
+irr_label = Label(root, text = "IRR", font = ("맑은고딕", 16, "bold"))
+irr_label.grid(column=0, row=7, sticky="w", padx=10)
+
+# 계산 버튼
+cal_button = Button(root, text="비교하기", height=3, bg="#63aff6", fg="navy") # TODO: command 추가해야함
+cal_button.grid(row=1, column=4, rowspan=4, padx=10, sticky="ns")
+
+# 사업 이름 추출 실험(완) ==============================
+'''
 newBusiness = Business(None, None, None, None)
 
 result_Label = Label(root, text="사업의 이름")
@@ -95,9 +132,7 @@ def whatName():
 # 비교하기 버튼
 button = Button(root, text="비교하기", command=whatName)
 button.grid(column=1, row=5)
-
-
-
+'''
 
 gameCorporation = Business(None, None, None, None)
 
